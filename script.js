c451e8cdc49b03b73ef9961b977cdbf66cf1e96e
bigbossbry <brycmramirez@gmail.com>
2022-10-22 02:26:56 +0800

    Finish computer choice function and user choice prompt.

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let number = Math.floor((Math.random() * 3));
+    if (number == 0) {
+        return "Rock";
+    } else if (number == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
