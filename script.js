e5e33d3ff1e555e881f23f4d82f585780bdfe954
bigbossbry <brycmramirez@gmail.com>
2022-10-22 23:33:27 +0800

    Create playRound function.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 function getComputerChoice() {
     let number = Math.floor((Math.random() * 3));
     if (number == 0) {
-        return computerSelection = "Rock";
+        return computerSelection = "rock";
     } else if (number == 1) {
-        return computerSelection = "Paper";
+        return computerSelection = "paper";
     } else {
-        return computerSelection = "Scissors";
+        return computerSelection = "scissors";
     }
-}
+};
 
 let playerSelection = prompt("Select your choice.", "");
 
-function playround(playerSelection, computerSelection) {
+function capitalize(word1) {
+    let lower = word1.toLowerCase();
+    let letter = lower.substr(0,1);
+    let wordLength = word1.length;
+    return letter.toUpperCase() + lower.slice(1,wordLength);
+}
 
+function playRound(playerSelection, computerSelection) {
+    switch (true) {
+        case (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "rock"):
+            console.log(`Player wins. ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`);
+            break;
+
+        case (playerSelection === computerSelection):
+            console.log(`It's a tie. You and the computer both chose ${playerSelection}`)
+            break;
+
+        default:
+            console.log(`You lose. ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`)
+    }
 }
