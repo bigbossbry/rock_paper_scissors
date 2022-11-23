2859fd040bfbe102ba5b2f73f021447a1be7b8d9
bigbossbry <brycmramirez@gmail.com>
2022-11-23 18:38:48 +0800

    Add buttons for the player choices and write a functional playround code.

--- script.js
+++ script.js
@@ -1,54 +1,99 @@
+//Computer selection
 function getComputerChoice() {
     let number = Math.floor((Math.random() * 3));
     if (number == 0) {
-        return computerSelection = "rock";
+        return computerSelection = "Rock";
     } else if (number == 1) {
-        return computerSelection = "paper";
+        return computerSelection = "Paper";
     } else {
-        return computerSelection = "scissors";
+        return computerSelection = "Scissors";
     }
 }
 
-let playerSelection = prompt("Select your choice.", "");
 
+// Player selection functions
+let playerSelection;
+
+function selectRock () {
+    playerSelection = "Rock";
+}
+
+function selectPaper () {
+    playerSelection = "Paper";
+}
+
+function selectScissors () {
+    playerSelection = "Scissors";
+}
+
+// Capitalize the first letter
 function capitalize(word1) {
     let lower = word1.toLowerCase();
     let letter = lower.substr(0,1);
     let wordLength = word1.length;
     return letter.toUpperCase() + lower.slice(1,wordLength);
 }
+
 let playerScore=0;
 let computerScore=0;
 
 function playRound(playerSelection, computerSelection) {
     switch (true) {
-        case (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "rock"):
-            console.log(`Player wins. ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}. The score is ${playerScore} to ${computerScore}`);
-            return ++playerScore;
-            break;
+        case (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Rock"):
+            ++playerScore
+            console.log(`Player wins. ${playerSelection} beats ${computerSelection}. The score is ${playerScore} to ${computerScore}`);
+            return playerScore;
 
         case (playerSelection === computerSelection):
             console.log(`It's a tie. You and the computer both chose ${playerSelection}`);
             break;
 
         default:
-            console.log(`You lose. ${capitalize(computerSelection)} beats ${capitalize(playerSelection)} The score is ${playerScore} to ${computerScore}`);
-            return ++computerScore;
+            ++computerScore
+            console.log(`You lose. ${computerSelection} beats ${playerSelection} The score is ${playerScore} to ${computerScore}`);
+            return computerScore;
     }
 }
 
+function playRoundRock() {
+    selectRock();
+    getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+function playRoundPaper() {
+    selectPaper();
+    getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+function playRoundScissors() {
+    selectScissors();
+    getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+/*
 function game() {
     playerScore = 0;
     computerScore = 0;
 
     for (playerScore=0; playerScore < 3 && computerScore < 3;) {
-        playRound(prompt(playerSelection), getComputerChoice());
+        playRound(playerSelection, getComputerChoice());
         if (playerScore == 3) {
             return `The Player won.`;
-            break;
         } else if (computerScore == 3 ) {
             return `The Computer won.`;
-            break;
         }
      }
-}
+}*/
+
+// Buttons functionality
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+rockButton.addEventListener('click', playRoundRock);
+paperButton.addEventListener('click', playRoundPaper);
+scissorsButton.addEventListener('click', playRoundScissors);
+
+
